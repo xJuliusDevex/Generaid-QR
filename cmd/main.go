a1302afd8d82d4ebf9d881feca9fdf5f79f93817
Julius <jcesargarq22@gmail.com>
2025-03-27 09:12:56 -0500

    probando

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,13 @@
 package main
-func main(){
-	fmt.Printl("hello wordl")
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+)
+
+func main() {
+	Ap := app.New()
+	w := Ap.NewWindow("JLS-QR")
+	w.Resize(fyne.NewSize(300, 300))
+	w.ShowAndRun()
 }
